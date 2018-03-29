13980d1cf03d0841752b8145ddb0e36a697880e1
Maxim Ponomarov <maxponomarov@users.noreply.github.com>
2018-03-29 20:03:53 +0300

    addded sjfhkjsd

---
+++ system/controllers/main.py
@@ -0,0 +1,15 @@
+dfghdgdhkdf;lgks
+fg'ks
+f'gls
+dls
+dglk
+sd;kgs
+d;globalsd;fks
+d'fksd'fksd'fksd'fkss'dkf
+s'dkfsdkf;dkfs;dfk
+sd;floatsd
+209871092837019283
+12312
+3123123123
+afasfasd
+asdasd
